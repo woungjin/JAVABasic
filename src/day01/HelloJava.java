156480334080b8b0f9d542610491cdd2694ca9bc
jinwoung <74219139+woungjin@users.noreply.github.com>
2021-01-10 12:12:51 +0900

    "Hello World"

--- src/day01/HelloJava.java
+++ src/day01/HelloJava.java
@@ -13,11 +13,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 
 		
-		for(int i = 0 ; i<3; i++) {
-		int	sum= 1 ; 
-			System.out.println(i);
-			
-		}
+		System.out.println("Hellow World");
 		
 
 	}
