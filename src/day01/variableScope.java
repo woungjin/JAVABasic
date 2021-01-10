83e8588cbd9cda7af412a2889eed9e1bc889dc86
jinwoung <74219139+woungjin@users.noreply.github.com>
2021-01-10 12:17:18 +0900

    지역변수,전역변수

--- src/day01/variableScope.java
+++ src/day01/variableScope.java
@@ -5,11 +5,15 @@ public class variableScope {
 	public static void main(String[] args) {
 		//변수의 범위
 		
-		int num1 = 10, num2= 20;
+		int num1 = 10, 
 		
 		if(true) {
 			int num3 = 30;
+			System.out.println(num1);
+			System.out.println(num3);
 		}
+		System.out.println(num3)
+			
 		
 		
 
