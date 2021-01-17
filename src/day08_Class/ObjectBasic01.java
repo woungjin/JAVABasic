210f4d5894444e56bf5120880396af3c7ffdf3d5
jinwoung <74219139+woungjin@users.noreply.github.com>
2021-01-17 23:08:10 +0900

    static을 활용한 계산기 작동

--- src/day08_Class/ObjectBasic01.java
+++ src/day08_Class/ObjectBasic01.java
@@ -4,7 +4,6 @@ public class ObjectBasic01 {
 
 	public static void main(String[] args) {
 		// 기존에 사용하던 방식
-
 		System.out.println("----1번 계산기----\n : " +add(1) + "\n : " + add(2) + "\n : " + add(3));
 		System.out.println("----2번 계산기----\n : " +add2(10) + "\n : " + add2(20) + "\n : " + add2(30));
 	}
